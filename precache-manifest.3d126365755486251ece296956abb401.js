c8792757ccf5185a62fa365644dbc6b018a9cc10
Boomers <offiwallet@gmail.com>
2023-01-14 15:27:27 +0530

    Updates

---
+++ precache-manifest.3d126365755486251ece296956abb401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd051bd2131291d836015108b4dbc5d",
+    "url": "/LIBM/index.html"
+  },
+  {
+    "revision": "c71d92bf38a0f5b7e386",
+    "url": "/LIBM/static/css/main.1468b141.chunk.css"
+  },
+  {
+    "revision": "da8687a13837aeb71439",
+    "url": "/LIBM/static/js/2.5d0fbf15.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/LIBM/static/js/2.5d0fbf15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71d92bf38a0f5b7e386",
+    "url": "/LIBM/static/js/main.d11d24e1.chunk.js"
+  },
+  {
+    "revision": "15742e612aa4d331fdd5",
+    "url": "/LIBM/static/js/runtime-main.9a62e919.js"
+  }
+]);
